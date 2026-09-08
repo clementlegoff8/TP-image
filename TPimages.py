8401743b82e1c5c91a4a5730d8c3c047136fa8c0
Clément Le Goff <clement.le_goff@etu.minesparis.psl.eu>
2026-09-08 10:27:02 +0200

    init git

--- TPimages.py
+++ TPimages.py
@@ -2,5 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 image = np.empty((91, 91, 3), dtype=np.uint8)
-plt.imshow(image)
-plt.show()
+
+image[:] = np.array([0,255,0])
+
+#print(image[0,0])
+#print(image[-1,-1])
+
+image[::10]= np.array([0,255,255])
+image[:,::10]= np.array([0,255,255])
+
+#plt.imshow(image)
+#plt.show()
+
+im = plt.imread("data/les-mines.jpg")
+im.flags.writeable
